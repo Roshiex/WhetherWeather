2652ca2edbcec44c21a951d660246f9e8ea4d3ee
Roisin <roisingleeson98@gmail.com>
2025-12-18 11:00:58 +0000

    updating

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+
   return {
     dir: {
       input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
+      output: "dist"
     }
   };
 };
