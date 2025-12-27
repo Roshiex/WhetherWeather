db2403f6bcbcdeeb4de32f9d3d7f5ae521edbded
Roisin <roisingleeson98@gmail.com>
2025-12-27 11:55:18 +0000

    updating for backup

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
 
   return {
     dir: {
       input: "src",
+      includes: "_includes",
+      data: "_data",
       output: "dist"
     }
   };
